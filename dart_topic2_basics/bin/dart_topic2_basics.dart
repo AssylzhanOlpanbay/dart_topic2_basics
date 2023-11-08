1806a01c6b4d461335bb3d67d85e01a154429e53
AsylzhanOlpanbay <asylzhan.olpanbay@mail.ru>
2023-11-08 15:48:43 +0600

    first commit

---
+++ dart_topic2_basics/bin/dart_topic2_basics.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dart_topic2_basics/dart_topic2_basics.dart' as dart_topic_2;
+
+void main(List<String> arguments) {
+  // final hoursSinceMudnight = DateTime.now().hour;
+  // final minutesSinceMudnight = DateTime.now().minute;
+  // final secondsSinceMudnight = DateTime.now().second;
+  // print(hoursSinceMudnight);
+  // print(minutesSinceMudnight);
+  // print(secondsSinceMudnight);
+
+
+  // int 
+  // for(int i = 1; 1 <= i && i <= 365; i++) {
+  //     if (i )
+  // }
+
+
+  // var y = stdin.readLineSync(encoding: utf8);
+  // if (y != null) {
+  //   var x = int.tryParse(y);
+  //   if (x != null) {
+  //       if(x < 12) {
+  //         x = x + 1;
+  //         print (x);
+  //     } else if (x >= 12) {
+  //       x = x - 12;
+  //       print(x + 1);
+  //     }
+  //   }
+   
+  // }
+
+  // var k = stdin.readLineSync(encoding: utf8);
+  // if (k != null) {
+  //   var x = int.tryParse(k);
+  //   if (x != null) {
+  //   if (x == 0) {
+  //   print("Monday");
+  // } else if (x == 1)  {
+  //   print("Tuesday");
+  // } else if (x == 2) {
+  //   print("Wednesday");
+  // } else if (x == 3) {
+  //   print("Thursday");
+  // } else if (x == 4) {
+  //   print("Friday");
+  // } else if (x == 5) {
+  //   print("Saturday");
+  // } else if (x == 6) {
+  //   print("Sunday");
+  // } else {
+  //   print("Error number");
+  // }
+  // }
+  // }
+
+  // final hoursSinceMudnight = DateTime.now().hour;
+  // final currentTime = DateTime.now();
+  // final twelve = currentTime.copyWith(hour: 12, minute: 0);
+  // print(hoursSinceMudnight);
+  // print(twelve);
+}
---
+++ dart_topic2_basics/test/dart_topic2_basics_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_topic2_basics/dart_topic2_basics.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
